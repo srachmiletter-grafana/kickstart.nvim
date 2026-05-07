afd9bd0d48bf5c03c7652ee03864a3fcf4765a4a
Sam Rachmil-Etter <sam.rachmiletter@grafana.com>
2026-05-07 09:03:12 -0500

    markdown rendering

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  ft = { 'markdown' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+}
